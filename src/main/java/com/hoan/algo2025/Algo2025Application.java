99039524178417c49b74390cf8c8924e2a1ad434
조근완 <kwjo@mqnic.com>
2025-04-22 10:49:21 +0900

    init

---
+++ src/main/java/com/hoan/algo2025/Algo2025Application.java
@@ -0,0 +1,13 @@
+package com.hoan.algo2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algo2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Algo2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hoan/algo2025/Algo2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoan.algo2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algo2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
