553e588443a3725152b8d824d078b32328853a9d
조근완 <kwjo@mqnic.com>
2025-08-07 10:51:10 +0900

    routine: 알고리즘 기초 1개

---
+++ src/main/java/com/hoan/algo2025/programmers/step0/조건에_맞게_수열_변환하기.java
@@ -0,0 +1,63 @@
+package com.hoan.algo2025.programmers.step0;
+
+/**
+ * 정수 배열 arr가 주어집니다.
+ * arr의 각 원소에 대해 값이 50보다 크거나 같은 짝수라면 2로 나누고,
+ * 50보다 작은 홀수라면 2를 곱하고 다시 1을 더합니다.
+ *
+ * 이러한 작업을 x번 반복한 결과인 배열을 arr(x)라고 표현했을 때,
+ * arr(x) = arr(x + 1)인 x가 항상 존재합니다.
+ * 이러한 x 중 가장 작은 값을 return 하는 solution 함수를 완성해 주세요.
+ *
+ * 단, 두 배열에 대한 "="는 두 배열의 크기가 서로 같으며, 같은 인덱스의 원소가 각각 서로 같음을 의미합니다.
+ *
+ * 1 ≤ arr의 길이 ≤ 1,000,000
+ * 1 ≤ arr의 원소의 값 ≤ 100
+ *
+ *
+ * arr	result
+ * [1, 2, 3, 100, 99, 98]	5
+ */
+public class 조건에_맞게_수열_변환하기 {
+    public static void main(String[] args) {
+        int [] arr = {1, 2, 3, 100, 99, 98};
+        int solution = solution(arr);
+
+        System.out.println("solution = " + solution);
+    }
+
+    // before 배열을 기억하고 있음.
+    public static int solution(int[] arr) {
+        int answer = -1;
+        int [] beforeArry = new int[arr.length];
+
+        while (!isSameArry(beforeArry, arr)) {
+            answer++;
+            beforeArry = arr.clone();
+
+            for(int i=0; i<arr.length; i++) {
+                if(arr[i] >= 50 && (arr[i] % 2) == 0) {
+                    arr[i] = (arr[i] / 2);
+                }
+
+                if(arr[i] < 50 && (arr[i] % 2) != 0) {
+                    arr[i] = (arr[i] * 2) + 1;
+                }
+            }
+
+        }
+
+
+        return answer;
+    }
+
+    private static boolean isSameArry(int[] beforeArry, int[] arr) {
+        for(int i=0; i<arr.length; i++) {
+            if(beforeArry[i] != arr[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
