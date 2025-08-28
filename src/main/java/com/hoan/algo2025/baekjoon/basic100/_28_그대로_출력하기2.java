31330859b439671636bbfbdf112f4afaa67a99b5
조근완 <HoanStore701@gmail.com>
2025-08-28 18:04:12 +0900

    routine: 0828 - 3

--- src/main/java/com/hoan/algo2025/baekjoon/basic100/_28_그대로_출력하기2.java
+++ src/main/java/com/hoan/algo2025/baekjoon/basic100/_28_그대로_출력하기2.java
@@ -2,7 +2,7 @@ package com.hoan.algo2025.baekjoon.basic100;
 
 import java.util.Scanner;
 
-public class _28_그대로_출력하기 {
+public class _28_그대로_출력하기2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
