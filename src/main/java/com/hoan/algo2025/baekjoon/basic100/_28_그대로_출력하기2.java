8cf1542e886381c99868014594424e697fd58105
조근완 <HoanStore701@gmail.com>
2025-08-28 18:01:25 +0900

    routine: 0828 매일 알고리즘

---
+++ src/main/java/com/hoan/algo2025/baekjoon/basic100/_28_그대로_출력하기2.java
@@ -0,0 +1,15 @@
+package com.hoan.algo2025.baekjoon.basic100;
+
+import java.util.Scanner;
+
+public class _28_그대로_출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+
+    }
+}
---
+++ src/main/java/com/hoan/algo2025/baekjoon/basic100/_29_더하기싸이클.java
@@ -0,0 +1,4 @@
+package com.hoan.algo2025.baekjoon.basic100;
+
+public class _29_더하기싸이클 {
+}
