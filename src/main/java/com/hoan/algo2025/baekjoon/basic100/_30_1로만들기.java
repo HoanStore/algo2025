d7de9c5fc63a560e2ccc406c8159c84745d36ca7
조근완 <HoanStore701@gmail.com>
2025-08-29 14:13:16 +0900

    routine: 0829 ~30/100

---
+++ src/main/java/com/hoan/algo2025/baekjoon/basic100/_30_1로만들기.java
@@ -0,0 +1,35 @@
+package com.hoan.algo2025.baekjoon.basic100;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class _30_1로만들기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine().trim());
+
+        int [] dp = new int[N+1];
+        int [] prev = new int[N+1];
+        dp[1] = 0;
+        prev[1] = 0;
+
+        for(int i=2; i<=N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            prev[i] = i - 1;
+
+            if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
+                dp[i] = dp[i / 2] + 1;
+                prev[i] = i / 2;
+            }
+            if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
+                dp[i] = dp[i / 3] + 1;
+                prev[i] = i / 3;
+            }
+        }
+
+        System.out.println(dp[N]);
+
+    }
+}
