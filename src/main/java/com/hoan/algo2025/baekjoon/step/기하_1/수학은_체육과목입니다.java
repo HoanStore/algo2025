f2abc18795e4d4108aea0bdf5976a193a49cb41e
조근완 <HoanStore701@gmail.com>
2025-10-24 13:57:11 +0900

    routine: 1024 - 백준 단계별 풀기

---
+++ src/main/java/com/hoan/algo2025/baekjoon/step/기하_1/수학은_체육과목입니다.java
@@ -0,0 +1,12 @@
+package com.hoan.algo2025.baekjoon.step.기하_1;
+
+import java.util.Scanner;
+
+public class 수학은_체육과목입니다 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+
+        System.out.println(A * 4);
+    }
+}
---
+++ src/main/java/com/hoan/algo2025/baekjoon/step/기하_1/직사각형.java
@@ -0,0 +1,13 @@
+package com.hoan.algo2025.baekjoon.step.기하_1;
+
+import java.util.Scanner;
+
+public class 직사각형 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A * B);
+    }
+}
