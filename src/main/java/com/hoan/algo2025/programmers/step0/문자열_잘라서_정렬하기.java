2407bb8703902f3067fe2e325e3574db0c3c9bd1
조근완 <HoanStore701@gmail.com>
2025-11-15 13:24:38 +0900

    routine: 매일 알고리즘

---
+++ src/main/java/com/hoan/algo2025/programmers/step0/문자열_잘라서_정렬하기.java
@@ -0,0 +1,46 @@
+package com.hoan.algo2025.programmers.step0;
+
+import java.util.Arrays;
+
+/**
+ * 문제 설명
+ * 문자열 myString이 주어집니다. "x"를 기준으로 해당 문자열을 잘라내 배열을 만든 후
+ * 사전순으로 정렬한 배열을 return 하는 solution 함수를 완성해 주세요.
+ *
+ * 단, 빈 문자열은 반환할 배열에 넣지 않습니다.
+ *
+ * 제한사항
+ * 1 ≤ myString ≤ 100,000
+ * myString은 알파벳 소문자로 이루어진 문자열입니다.
+ * 입출력 예
+ * myString	result
+ * "axbxcxdx"	["a","b","c","d"]
+ * "dxccxbbbxaaaa"	["aaaa","bbb","cc","d"]
+ *
+ *
+ * 반례입니다.
+ * 입력값 〉 "bbxba"
+ * 기댓값 〉 ["ba", "bb"]
+ */
+public class 문자열_잘라서_정렬하기 {
+    public static void main(String[] args) {
+        String myString = "dxccxbbbxaaaa";
+
+        String[] solution = solution(myString);
+
+        System.out.println(Arrays.toString(solution));
+    }
+
+
+    public static String[] solution(String myString) {
+        String[] xes = myString.split("x");
+        Arrays.sort(xes);
+
+
+        String[] filteredArray = Arrays.stream(xes)
+                .filter(s -> !s.isEmpty())
+                .toArray(String[]::new);
+
+        return filteredArray;
+    }
+}
