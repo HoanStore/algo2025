e68a2124145858d389fa2a7749a92248f0138ab1
조근완 <HoanStore701@gmail.com>
2025-12-04 22:11:27 +0900

    routine: 백준 - 매일 알고리즘

---
+++ src/main/java/com/hoan/algo2025/baekjoon/step/브루트_포스/수학은_비대면_강의입니다.java
@@ -0,0 +1,12 @@
+package com.hoan.algo2025.baekjoon.step.브루트_포스;
+
+import java.util.Scanner;
+
+public class 수학은_비대면_강의입니다 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+    }
+}
