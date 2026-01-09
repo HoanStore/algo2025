86730913e8159ff5a734ddc46cb2e05186a8328e
HoanStore <HoanStore701@gmail.com>
2026-01-09 18:53:33 +0900

    routine: 자바 퀴즈 시작

---
+++ src/main/java/com/hoan/algo2025/javaquiz/quiz01.java
@@ -0,0 +1,13 @@
+package com.hoan.algo2025.javaquiz;
+
+public class quiz01 {
+
+    public static void main(String[] args) {
+        String s1 = "java";
+        String s2 = "java";
+
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+    }
+
+}
